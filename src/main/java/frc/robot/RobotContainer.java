3094c422ae699621fd35fdda4591d5eb647b7d41
Jeffrey Wang <jewang@beachwoodstudents.org>
2020-01-20 20:21:24 -0500

    controllable motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.Joystick;
@@ -29,7 +30,15 @@ import edu.wpi.first.wpilibj.Joystick;
  * Left X-axis: 0
  * Left Y-axis: 1
  * 
- */
+ * POV:
+ *      0
+ *      ^
+ *      |
+ * 270<- -> 90
+ *      |
+ *      v
+ *     180
+ * /
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -53,6 +62,11 @@ public class RobotContainer {
   private final JoystickButton AButton = new JoystickButton(ControllerDrive,1);
   ////private final JoystickButton XButton = new JoystickButton(ControllerDrive,3);
   private final JoystickButton YButton = new JoystickButton(ControllerDrive,4);
+  private final POVButton POVTopRight = new POVButton(ControllerDrive, 45);
+  private final POVButton POVBottomLeft = new POVButton(ControllerDrive, 135);
+  private final POVButton POVBottomRight = new POVButton(ControllerDrive, 225);
+  private final POVButton POVTopLeft = new POVButton(ControllerDrive, 315);
+
   //***************************************************************************/
   //COMMANDS INIT & CONSTRUCTED BELOW:
   //***************************************************************************/
@@ -60,8 +74,11 @@ public class RobotContainer {
   // final MattDrive ActivateMattDrive = new MattDrive(RobotDriveMotors,ControllerDrive,1,4);
   //private final SetColor ColorSensorUsed = new SetColor();
   //private final SetTurretRotation ActivateTurret = new SetTurretRotation(Turret, ControllerDrive, 0, 1);
-  private final FalconClosedLoop ClosedLoop = new FalconClosedLoop(12,0,30,ControlMode.Position); //The motor we use is yet to be determined.
-  private final EnableFalconVelocityClosedLoop ActivateClosedLoop = new EnableFalconVelocityClosedLoop(ClosedLoop,3000);
+  private final FalconClosedLoop ClosedLoopTop = new FalconClosedLoop(12,0,30,ControlMode.Velocity); //The motor we use is yet to be determined.
+  private final FalconClosedLoop ClosedLoopBottom = new FalconClosedLoop(10,0,30,ControlMode.Velocity); 
+  //  private final EnableFalconVelocityClosedLoop ActivateClosedLoop = new EnableFalconVelocityClosedLoop(ClosedLoop,3000);
+  //private final PrintControllerPOV PrintController = new PrintControllerPOV(ControllerDrive);
+  private final MultipleFalconClosedVelocity shooterMultiVelocity = new MultipleFalconClosedVelocity(new double[]{0.0,0.0},new FalconClosedLoop[]{ClosedLoopTop,ClosedLoopBottom});
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
@@ -79,6 +96,11 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    POVTopRight.whenPressed(new InstantCommand(() -> {shooterMultiVelocity.incrementSpecificVelocity(0,500);}));
+    POVTopLeft.whenPressed(new InstantCommand(() -> {shooterMultiVelocity.incrementSpecificVelocity(0,-500);}));
+    POVBottomRight.whenPressed(new InstantCommand(() -> {shooterMultiVelocity.incrementSpecificVelocity(1,500);}));
+    POVBottomLeft.whenPressed(new InstantCommand(() -> {shooterMultiVelocity.incrementSpecificVelocity(1,-500);}));
+
    // YButton.whenPressed(new InstantCommand(() -> {ActivateClosedLoop.incrementPosition(1);}));
     //AButton.whenPressed(new InstantCommand(() -> {ActivateClosedLoop.incrementPosition(-1);}));
   }
@@ -91,9 +113,8 @@ public class RobotContainer {
    */
   public Command getTeleopCommand() {
     ParallelCommandGroup ContinuousTeleop = new ParallelCommandGroup();
-    //ContinuousTeleop.addCommands(new InstantC);
     //test.set(ControlMode.PercentOutput,0.5);
-    ContinuousTeleop.addCommands(ActivateClosedLoop);
+    ContinuousTeleop.addCommands(shooterMultiVelocity);
     return ContinuousTeleop;
   }
 }
