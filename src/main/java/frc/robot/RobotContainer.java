8781e1810cc4fc15a7c12f6d262f7aada6ed975e
optimus222 <optimus222@users.noreply.github.com>
2020-01-20 20:19:07 -0500

    Commented Turret Code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -107,8 +107,8 @@ public class RobotContainer {
     //AButton.whenPressed(new InstantCommand(() -> {ActivateClosedLoop.incrementPosition(-1);}));
 
     TurretGroup.addCommands(Finding,Focusing);
-    AButton.whenPressed(TurretGroup);
-    XButton.whenPressed(new InstantCommand(Focusing::change));
+    //AButton.whenPressed(TurretGroup);
+    //XButton.whenPressed(new InstantCommand(Focusing::change));
     //YButton.whenPressed(new InstantCommand(HatchSolenoid::toggleHatchState, HatchSolenoid));
 
 
